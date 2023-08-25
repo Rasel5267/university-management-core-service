8427566a8787daab609ce87dfb92d9042e3a3d4c
Rasel5267 <mahadiul09@gmail.com>
2023-08-25 11:57:28 +0600

    student and faculty create and get functionality

---
+++ src/app/modules/faculty/faculty.service.ts
@@ -0,0 +1,105 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Faculty, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import prisma from '../../../shared/prisma';
+import { facultySearchableFields } from './faculty.constant';
+import { IFacultyFilterRequest } from './faculty.interface';
+
+const insertIntoDB = async (data: Faculty): Promise<Faculty> => {
+  const result = await prisma.faculty.create({
+    data,
+  });
+
+  return result;
+};
+
+const getAllFromDB = async (
+  filters: IFacultyFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<Faculty[]>> => {
+  const { limit, page, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: facultySearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+          equals: (filterData as any)[key].toLowerCase(),
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.FacultyWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.faculty.findMany({
+    include: {
+      academicFaculty: true,
+      academicDepartment: true,
+    },
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.faculty.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
+const getDataById = async (id: string): Promise<Faculty | null> => {
+  const result = await prisma.faculty.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return result;
+};
+
+const deleteByIdFromDB = async (id: string): Promise<Faculty> => {
+  const result = await prisma.faculty.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+
+export const FacultyService = {
+  insertIntoDB,
+  getAllFromDB,
+  getDataById,
+  deleteByIdFromDB,
+};
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,65 @@
+import { Student } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { studentFilterableFields } from './student.constant';
+import { StudentService } from './student.service';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentService.insertIntoDB(req.body);
+
+  sendResponse<Student>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student Created successfully',
+    data: result,
+  });
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentFilterableFields);
+  const options = pick(req.query, paginationFields);
+
+  const result = await StudentService.getAllFromDB(filters, options);
+  sendResponse<Student[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getDataById = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const result = await StudentService.getDataById(id);
+  sendResponse<Student>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student retrieved successfully',
+    data: result,
+  });
+});
+
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const result = await StudentService.getDataById(id);
+  sendResponse<Student>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student deleted successfully',
+    data: result,
+  });
+});
+
+export const StudentController = {
+  insertIntoDB,
+  getAllFromDB,
+  getDataById,
+  deleteByIdFromDB,
+};
