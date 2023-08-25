7434b0cc1a4027734c2c1d02efbf5261b4d760fa
Rasel5267 <mahadiul09@gmail.com>
2023-08-25 13:15:14 +0600

    add user role base access

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middleware/auth';
 import validateRequest from '../../middleware/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
@@ -8,16 +10,22 @@ const router = express.Router();
 router.post(
   '/',
   validateRequest(AcademicSemesterValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.insertIntoDB
 );
 
 router.patch(
   '/:id',
   validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.updateOneInDB
 );
 
-router.delete('/:id', AcademicSemesterController.deleteFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteFromDB
+);
 
 router.get('/:id', AcademicSemesterController.getDataById);
 
--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middleware/auth';
 import validateRequest from '../../middleware/validateRequest';
 import { FacultyController } from './faculty.controller';
 import { FacultyValidation } from './faculty.validation';
@@ -8,16 +10,22 @@ const router = express.Router();
 router.post(
   '/',
   validateRequest(FacultyValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   FacultyController.insertIntoDB
 );
 
 router.patch(
   '/:id',
   validateRequest(FacultyValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   FacultyController.updateOneInDB
 );
 
-router.delete('/:id', FacultyController.deleteFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.deleteFromDB
+);
 
 router.get('/:id', FacultyController.getDataById);
 
